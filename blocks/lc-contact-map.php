5867ed6ea9ffd94b61c514c13caa68c2c7427528
LamcatUK <git@lamcat.co.uk>
2025-11-30 20:05:17 +0000

    contact

--- blocks/lc-contact-map.php
+++ blocks/lc-contact-map.php
@@ -4,6 +4,8 @@
         <h2>Find Us</h2>
         <div class="row g-4">
             <div class="col-md-5">
+                <div class="mb-4">The practice is located at 50 Ripple Road, close to Barking station and local bus routes.</div>
+                <div class="mb-4">Walk-ins are accepted when available, though appointments are recommended during busy periods.</div>
                 <div class="open card mb-4">
                     <h3>Opening Hours</h3>
                     <?= do_shortcode( '[lc_open_short_ajax]' ); ?>
