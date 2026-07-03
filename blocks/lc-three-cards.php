192b9d4f48c6321ff7e4b3b915ccf80712a7e082
LamcatUK <git@lamcat.co.uk>
2026-07-03 22:02:07 +0100

    three cards

--- blocks/lc-three-cards.php
+++ blocks/lc-three-cards.php
@@ -8,11 +8,14 @@
 defined( 'ABSPATH' ) || exit;
 
 $class_name = $block['className'] ?? 'mt-5';
+$data    = $block['data'] ?? array();
+$show_bg = isset( $data['show_background'] ) ? (bool) $data['show_background'] : true;
 
-// Count how many cards are defined.
+// Count how many cards are defined (based on title).
 $card_count = 0;
 for ( $i = 1; $i <= 5; $i++ ) {
-	if ( get_field( 'card_' . $i . '_link' ) ) {
+	$title = $data[ 'card_' . $i . '_title' ] ?? '';
+	if ( $title ) {
 		++$card_count;
 	}
 }
@@ -29,19 +32,20 @@ $card_size  = $card_sizes[ $card_count ] ?? 'col-md-4';
 
 ?>
 <!-- cards -->
-<section class="three_cards py-5 <?= esc_attr( $class_name ); ?>">
+<section class="three_cards py-5 <?= esc_attr( $class_name ); ?><?= $show_bg ? '' : ' no-bg'; ?>">
 	<div class="container-xl">
 		<div class="row g-4 justify-content-center">
 			<?php
 			for ( $i = 1; $i <= 5; $i++ ) {
-				$link = get_field( 'card_' . $i . '_link' );
-				if ( ! $link ) {
+				$title = $data[ 'card_' . $i . '_title' ] ?? '';
+				$body  = $data[ 'card_' . $i . '_body' ] ?? '';
+				$link  = $data[ 'card_' . $i . '_link' ] ?? '';
+				$icon_id = $data[ 'card_' . $i . '_icon' ] ?? null;
+				$icon    = $icon_id ? wp_get_attachment_image_url( $icon_id, 'large' ) : '';
+
+				if ( ! $title && ! $body && ! $link && ! $icon_id ) {
 					continue;
 				}
-
-				$icon  = wp_get_attachment_image_url( get_field( 'card_' . $i . '_icon' ), 'large' );
-				$title = get_field( 'card_' . $i . '_title' );
-				$body  = get_field( 'card_' . $i . '_body' );
 
 				// For 5 cards: first 3 are col-md-4, last 2 are col-md-6.
 				if ( 5 === $card_count ) {
@@ -52,18 +56,24 @@ $card_size  = $card_sizes[ $card_count ] ?? 'col-md-4';
 				?>
 			<div class="<?= esc_attr( $current_card_size ); ?>">
 				<div class="card">
+					<?php if ( $icon ) { ?>
 					<div class="card__icon">
 						<img src="<?= esc_url( $icon ); ?>" alt="<?= esc_attr( $title ); ?>" width="267" height="150">
 					</div>
+					<?php } ?>
 					<div class="card__bottom">
 						<h3><?= esc_html( $title ); ?></h3>
+						<?php if ( $body ) { ?>
 						<p><?= esc_html( $body ); ?></p>
+						<?php } ?>
 					</div>
+					<?php if ( $link && ! empty( $link['url'] ) ) { ?>
 					<div class="card__button">
 						<a href="<?= esc_url( $link['url'] ); ?>"
 							target="<?= esc_attr( $link['target'] ); ?>"
 							class="btn btn-primary"><?= esc_html( $link['title'] ); ?></a>
 					</div>
+					<?php } ?>
 				</div>
 			</div>
 				<?php
@@ -71,20 +81,21 @@ $card_size  = $card_sizes[ $card_count ] ?? 'col-md-4';
 			?>
 		</div>
 		<?php
-		if ( get_field( 'after_text' ) ) {
+		$after_text = get_field( 'after_text' );
+		if ( $after_text ) {
 			?>
 		<div class="text-center mt-5">
-			<?= esc_html( get_field( 'after_text' ) ); ?>
+			<?= esc_html( $after_text ); ?>
 		</div>
 			<?php
 		}
-		if ( get_field( 'after_cta' ) ) {
-			$c = get_field( 'after_cta' );
+		$after_cta = get_field( 'after_cta' );
+		if ( $after_cta ) {
 			?>
 		<div class="text-center mt-4">
-			<a href="<?= esc_url( $c['url'] ); ?>"
-				target="<?= esc_attr( $c['target'] ); ?>"
-				class="btn btn-primary"><?= esc_html( $c['title'] ); ?></a>
+			<a href="<?= esc_url( $after_cta['url'] ); ?>"
+				target="<?= esc_attr( $after_cta['target'] ); ?>"
+				class="btn btn-primary"><?= esc_html( $after_cta['title'] ); ?></a>
 		</div>
 			<?php
 		}
--- blocks/lc-two-cols.php
+++ blocks/lc-two-cols.php
@@ -1,19 +1,27 @@
 <?php
-$feature = get_field('featured')[0] == 'Yes' ? 'card--featured' : '';
+/**
+ * Two Columns Block
+ *
+ * @package LC-iology2025
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+$feature = 'Yes' === get_field( 'featured' )[0] ? 'card--featured' : '';
 ?>
 <!-- two_cols -->
 <section class="two_cols">
-    <div class="container-xl">
-        <div class="row g-4">
-            <div class="col-md-8">
-                <div class="card">
-                    <?=get_field('left_content')?>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card <?=$feature?>">
-                    <?=get_field('right_content')?></div>
-            </div>
-        </div>
-    </div>
+	<div class="container-xl">
+		<div class="row g-4">
+			<div class="col-md-8">
+				<div class="card">
+					<?= get_field( 'left_content' ); ?>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="card <?= $feature; ?>">
+					<?= get_field( 'right_content' ); ?></div>
+			</div>
+		</div>
+	</div>
 </section>
