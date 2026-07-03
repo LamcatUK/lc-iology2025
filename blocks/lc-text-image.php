18b36bdc632ae1b1373df0c923e6e06a6a2cf97a
LamcatUK <git@lamcat.co.uk>
2026-07-03 21:28:17 +0100

    text image split

--- blocks/lc-text-image.php
+++ blocks/lc-text-image.php
@@ -7,18 +7,21 @@
 
 defined( 'ABSPATH' ) || exit;
 
-$order_image = ( 'text image' === get_field('order') ) ? 'order-1 order-lg-2' : 'order-1 order-lg-1';
-$order_text  = ( 'text image' === get_field('order') ) ? 'order-2 order-lg-1' : 'order-2 order-lg-2';
+$order_image = ( 'text image' === get_field( 'order' ) ) ? 'order-1 order-lg-2' : 'order-1 order-lg-1';
+$order_text  = ( 'text image' === get_field( 'order' ) ) ? 'order-2 order-lg-1' : 'order-2 order-lg-2';
+$split       = get_field( 'split' ) ? get_field( 'split' ) : '8:4';
+$text_col    = ( '6:6' === $split ) ? 'col-md-6' : 'col-md-8';
+$image_col   = ( '6:6' === $split ) ? 'col-md-6' : 'col-md-4';
 ?>
 <section class="text_image mb-4">
 	<div class="container-xl">
 		<div class="row g-4">
-			<div class="col-md-8 <?= esc_attr( $order_text ); ?>">
+			<div class="<?= esc_attr( $text_col ); ?> <?= esc_attr( $order_text ); ?>">
 				<div class="card">
 					<?= wp_kses_post( get_field( 'text_content' ) ); ?>
 				</div>
 			</div>
-			<div class="col-md-4 <?= esc_attr( $order_image ); ?>">
+			<div class="<?= esc_attr( $image_col ); ?> <?= esc_attr( $order_image ); ?>">
 				<div class="card-image">
 					<?= wp_get_attachment_image( get_field( 'image' ), 'full' ); ?>
 				</div>
